29a07982357aeac18193939837f3dc5a1d2b5187
jim0123 <jim0123@gmail.com>
2023-07-04 19:40:05 +0800

    aaa

---
+++ profileUpdate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "hello"
+cp -f /Users/JayGee/.config/clash/profiles/1668790216617.yml /Users/JayGee/Documents/GitHub/vault/1668790216617.yml
+cd /Users/JayGee/Documents/GitHub/vault/
+git add .
+git commit
+git push
