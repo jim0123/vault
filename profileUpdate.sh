e0f20b0210e9c855cbdb92e1b2ac8e0f94e6bf49
jim0123 <jim0123@gmail.com>
2023-07-08 20:56:46 +0800

    aa

--- profileUpdate.sh
+++ profileUpdate.sh
@@ -3,5 +3,6 @@ echo "hello"
 cp -f /Users/JayGee/.config/clash/profiles/1668790216617.yml /Users/JayGee/Documents/GitHub/vault/1668790216617.yml
 cd /Users/JayGee/Documents/GitHub/vault/
 git add .
-git commit
+git commit --allow-empty-message
 git push
+osascript -e 'tell application "Terminal" to close (every window whose name contains "profileUpdate.sh")'
