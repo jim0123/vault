619f6286e9f1520da0d508c0bf900a700dea8f4c
jim0123 <jim0123@gmail.com>
2023-07-17 16:49:44 +0800

    Mon Jul 17 16:49:44 CST 2023

--- watcher.sh
+++ watcher.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fswatch -0 /Users/JayGee/.config/clash/profiles/1668790216617.yml | xargs -0 -n 1 /Users/JayGee/Documents/GitHub/vault/profileUpdate.sh
+fswatch -0 /Users/JayGee/.config/clash/profiles/1668790216617.yml | xargs -0 -n 1 /Users/JayGee/Documents/GitHub/vault/profileUpdate.sh &
